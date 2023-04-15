24e1d05e4ffa86ede2b0582d8b8e7544e5e0e521
Divya Raj <divyaraj7612@gmail.com>
2023-04-16 02:38:57 +0530

    basic login page created

--- medicose-frontend/pages/login.js
+++ medicose-frontend/pages/login.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import Logindesign from "../components/Logindesign"
+import React from "react";
+import Logindesign from "../components/Logindesign";
+import Image from "next/image";
 
 function login() {
   return (
     <div className="loginContainer">
       <div className="loginDesignContainer">
-      <Logindesign />
+        <Logindesign />
       </div>
       <div className="loginFormContainer">
-        
+        <div className="okGestureIcon">
+          <Image src="/healthy.png" width={95} height={111} />
+        </div>
+        <div className="greet">Hello ! Welcome Back</div>
+        <div>
+          <div className="inputLabel">Username</div>
+          <input type="text" placeholder="Enter your username" />
+        </div>
+        <div>
+          <div className="inputLabel">Password</div>
+          <input type="password" placeholder="Enter your Password" />
+        </div>
+        <div className="checkboxContainer">
+          <div style={{ display: "flex", flexDirection: "row", height: '18px' }}>
+            <input type="checkbox" />
+            <label>Remember me</label>
+          </div>
+          <div style={{color: "#089996", cursor: "pointer"}}>Forgot your password?</div>
+        </div>
+        <button style={{ backgroundColor: "#089996", color: "#FFFFFF" }}>
+          Login
+        </button>
+        <button>
+          <Image
+            src="/googleLogo.png"
+            width={30}
+            height={35}
+            style={{ marginRight: "16px" }}
+          />
+          Login with Google
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default login
+export default login;
