ce802572c0fdaffbbc89bdd6e766ff743d78d8da
Divya Raj <divyaraj7612@gmail.com>
2023-04-21 05:21:28 +0530

    forms modified

--- medicose-frontend/components/Forms.js
+++ medicose-frontend/components/Forms.js
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-export default function Loginform() {
+export default function Loginform(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="greet">Hello ! Welcome Back</div>
+      <div>
+        <div className="inputLabel">Username</div>
+        <input type="text" placeholder="Enter your username" />
+      </div>
+      <div>
+        <div className="inputLabel">Password</div>
+        <input type="password" placeholder="Enter your Password" />
+      </div>
+      <div className="checkboxContainer">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            height: "18px",
+          }}
+        >
+          <input type="checkbox" />
+          <label>Remember me</label>
+        </div>
+        <div
+          style={{
+            color: "#089996",
+            cursor: "pointer",
+          }}
+        >
+          Forgot your password?
+        </div>
+      </div>
+
+      <button
+        style={{
+          backgroundColor: "#089996",
+          color: "#FFFFFF",
+        }}
+      >
+        Login
+      </button>
+      <hr />
+      <button
+        style={{
+          backgroundColor: "#FFFFFF",
+          color: "black",
+        }}
+      >
+        <Image
+          src="/googleLogo.png"
+          width={30}
+          height={35}
+          style={{ marginRight: "16px" }}
+        />
+        Login with Google
+      </button>
+      <hr />
+      <div>
+        <text> Don’t have an account?</text>
+        <span
+          onClick={props.NavigateToSignup}
+          style={{
+            cursor: "pointer",
+            color: "#089996",
+            marginLeft: "10px",
+          }}
+        >
+          Create Account
+        </span>
+      </div>
+    </>
+  );
 }
+
+function Signupform(props) {
+  return (
+    <>
+      <div className="greet">Create your account</div>
+      <div>
+        <div className="inputLabel">Username</div>
+        <input type="text" placeholder="Enter your Name" />
+      </div>
+      <div>
+        <div className="inputLabel">Email</div>
+        <input type="email" placeholder="Enter your Email" />
+      </div>
+      <div>
+        <div className="inputLabel">Password</div>
+        <input type="password" placeholder="Enter your Password" />
+      </div>
+      <div>
+        <div className="inputLabel">Confirm Password</div>
+        <input type="password" placeholder="Confirm password" />
+      </div>
+      <button onClick={props.NavigateToOtpform}>Signup</button>
+    </>
+  );
+}
+
+function Otpform() {
+  return (
+    <>
+      <div className="greet">Create your account</div>
+      <div>
+        <div className="inputLabel">OTP</div>
+        <input type="number" placeholder="Enter One Time Password" />
+      </div>
+      <button>Verify</button>
+    </>
+  );
+}
+
+export { Signupform, Otpform };
--- medicose-frontend/pages/login.js
+++ medicose-frontend/pages/login.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Logindesign from "../components/Logindesign";
 import Image from "next/image";
+import Loginform from "../components/Forms";
+import { Signupform,Otpform } from "../components/Forms";
 import { useState } from "react";
 
 function login() {
   const [renderId, setrenderId] = useState(0);
+  function NavigateToSignup() {
+    setrenderId(1);
+  }
+  function NavigateToOtpform() {
+    setrenderId(2);
+  }
   return (
     <div className="loginContainer">
       <div className="loginDesignContainer">
@@ -14,63 +22,9 @@ function login() {
         <div className="okGestureIcon">
           <Image src="/healthy.png" width={95} height={111} />
         </div>
-        {renderId == 0 && (
-          <>
-            <div className="greet">Hello ! Welcome Back</div>
-            <div>
-              <div className="inputLabel">Username</div>
-              <input type="text" placeholder="Enter your username" />
-            </div>
-            <div>
-              <div className="inputLabel">Password</div>
-              <input type="password" placeholder="Enter your Password" />
-            </div>
-            <div className="checkboxContainer">
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  height: "18px",
-                }}
-              >
-                <input type="checkbox" />
-                <label>Remember me</label>
-              </div>
-              <div
-                style={{
-                  color: "#089996",
-                  cursor: "pointer",
-                }}
-              >
-                Forgot your password?
-              </div>
-            </div>
-
-            <button
-              style={{
-                backgroundColor: "#089996",
-                color: "#FFFFFF",
-              }}
-            >
-              Login
-            </button>
-            <hr />
-            <button>
-              <Image
-                src="/googleLogo.png"
-                width={30}
-                height={35}
-                style={{ marginRight: "16px" }}
-              />
-              Login with Google
-            </button>
-            <hr />
-            <div>
-              <span> Don’t have an account?</span>
-              <span>Create Account</span>
-            </div>
-          </>
-        )}
+        {renderId == 0 && <Loginform NavigateToSignup={NavigateToSignup} />}
+        {renderId == 1 && <Signupform NavigateToOtpform={NavigateToOtpform} />}
+        {renderId == 2 && <Otpform />}
       </div>
     </div>
   );
