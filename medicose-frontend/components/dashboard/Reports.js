6a5ee44fc6069211d98ca744bb3ff35b55f2a34b
Divya Raj <divyaraj7612@gmail.com>
2023-05-09 19:22:29 +0530

    api integrated for dashboard

--- medicose-frontend/components/dashboard/Reports.js
+++ medicose-frontend/components/dashboard/Reports.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 function Reports() {
+  const [Data, setData] = useState([]);
+
+  async function GetData() {
+    let Response = await axios.get(
+      "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Frssfeedstopstories.cms"
+    );
+    return Response;
+  }
+  GetData().then((Response) => {
+    setData(Response.data.items)
+  });
+
   return (
     <div className="reportsContainer">
       <div className="reportHeadingContainer">
@@ -19,38 +33,37 @@ function Reports() {
             style={{
               marginLeft: "-2.5rem",
             }}
+            alt="search Icon"
           />
         </div>
-        <div style={{display: "flex",
-      flexDirection:"row", gap:"1rem" }}>
+        <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
           <div className="filterContainer">
             <span>Filter</span>
-            <Image 
-            src="/filterIcon.png"
-            width={20}
-            height={20}
-            />
+            <Image src="/filterIcon.png" width={20} height={20} alt="filter Icon" />
           </div>
           <div className="filterContainer">
-          <span>Sort by</span>
-            <Image 
-            src="/sortbyIcon.png"
-            width={20}
-            height={20}
-            />
+            <span>Sort by</span>
+            <Image src="/sortbyIcon.png" width={20} height={20} alt="sortby Icon" />
           </div>
         </div>
       </div>
       <div className="reportContainerBottom">
-        <div className="patientReport">
-          <Image
-          src="/profilePic.png"
-          width={56}
-          height={56}
-          />
-          <div>patient"s Name</div>
-          <div>patient"s Id</div>
-        </div>
+        {Data &&
+          Data.map(({ profile_image, patient_name, patient_id }) => {
+            return (
+              <div className="patientReport">
+                <Image
+                  // src={profile_image === " " ? "/profilePic.png" : "/profile_image"}
+                  src="/profilePic.png"
+                  width={56}
+                  height={56}
+                  alt="profile Image"
+                />
+                <div>{patient_name}</div>
+                <div>{patient_id}</div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
