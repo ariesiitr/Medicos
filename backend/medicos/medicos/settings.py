1f3b91c97f7b75ab235cca7c5a900d210766c118
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-05-11 17:41:17 +0530

    adds order GET POST API
    adds order GET POST API

--- backend/medicos/medicos/settings.py
+++ backend/medicos/medicos/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+
 
 env = environ.Env()
 environ.Env.read_env()
@@ -23,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
     'Doctor.apps.DoctorConfig',
     'clinic.apps.ClinicConfig',
     'patient.apps.PatientConfig',
+    'order.apps.OrderConfig',
 
 ]
 
---
+++ backend/medicos/order/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import order_view
+urlpatterns = [
+    path('', order_view)
+]
