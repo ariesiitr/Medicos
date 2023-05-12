c2a1089a8306874e65be5f76c6737f749760aa91
Divya Raj <divyaraj7612@gmail.com>
2023-05-12 11:54:09 +0530

    modified report

--- medicose-frontend/components/dashboard/Reports.js
+++ medicose-frontend/components/dashboard/Reports.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import axios from "axios";
 import { useState } from "react";
 
-function Reports() {
-  const [Data, setData] = useState([]);
 
-  async function GetData() {
-    let Response = await axios.get(
-      "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Frssfeedstopstories.cms"
-    );
-    return Response;
-  }
-  GetData().then((Response) => {
-    setData(Response.data.items)
-  });
+function Reports() {
+  // const [Data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // useEffect(() => {
+  //   async function GetData() {
+  //     let Response = await axios.get("");
+  //     return Response;
+  //   }
+  //   GetData().then((Response) => {
+  //     setData(Response.data.items);
+  //   });
+  // }, []);
+  // console.log(Data);
 
   return (
     <div className="reportsContainer">
@@ -23,8 +26,9 @@ function Reports() {
         <div className="reportSeachboxContainer">
           <input
             type="search"
-            placeholder="Enter Patient name or Patient Id"
+            placeholder="Enter Patient name"
             className="reportSearchbox"
+            onChange={(e) => setSearch(e.target.value)}
           />
           <Image
             src="/searchIcon.png"
@@ -39,28 +43,43 @@ function Reports() {
         <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
           <div className="filterContainer">
             <span>Filter</span>
-            <Image src="/filterIcon.png" width={20} height={20} alt="filter Icon" />
+            <Image
+              src="/filterIcon.png"
+              width={20}
+              height={20}
+              alt="filter Icon"
+            />
           </div>
           <div className="filterContainer">
             <span>Sort by</span>
-            <Image src="/sortbyIcon.png" width={20} height={20} alt="sortby Icon" />
+            <Image
+              src="/sortbyIcon.png"
+              width={20}
+              height={20}
+              alt="sortby Icon"
+            />
           </div>
         </div>
       </div>
       <div className="reportContainerBottom">
         {Data &&
-          Data.map(({ profile_image, patient_name, patient_id }) => {
+          Data.filter((Data) => {
+            return search.toLowerCase() === ""
+              ? Data
+              : Data.first_name.toLowerCase().includes(search);
+          }).map(({ profile_image, first_name, id }) => {
+            console.log(profile_image);
             return (
-              <div className="patientReport">
+              <div className="patientReport" key={id}>
                 <Image
-                  // src={profile_image === " " ? "/profilePic.png" : "/profile_image"}
+                  // src={profile_image === undefined ?"/profilePic.png" : "/profile_image" }
                   src="/profilePic.png"
                   width={56}
                   height={56}
                   alt="profile Image"
                 />
-                <div>{patient_name}</div>
-                <div>{patient_id}</div>
+                <div>{first_name}</div>
+                <div>{id}</div>
               </div>
             );
           })}
