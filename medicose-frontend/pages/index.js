15fc3cf0c80348a9b7b45b4b03959643e2327c79
Divya Raj <divyaraj7612@gmail.com>
2023-05-12 11:34:50 +0530

    started working on payment file

--- medicose-frontend/pages/index.js
+++ medicose-frontend/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Chatpage from "../components/dashboard/Chatpage"
 import Reports from "../components/dashboard/Reports"
+import Payment from "../components/dashboard/Payment"
 
 export default function Home() {
   return (
     <div>
-  <Reports />
+  <Payment/>
     </div>
   )
 }
